e3cea756b1480c8dcd0982064a2bc8361c280600
chandler <chandler@napkinlabs.com>
2012-02-09 16:15:35 -0700

    renamed some variables to make it more clear

--- ebs-snapshot.php
+++ ebs-snapshot.php
@@ -10,22 +10,32 @@
          * we are only locking the MySQL tables, the volume can still have activity and
          * if you snapshot during activity you could end up with corrupted data.
          *
+         * Not responsible if you delete all the data.
+         *
+         * Requirements: PHP, AWS-API-TOOLS, php-pdo
+         *
+         *
          */
 
         //ENV Variables
         putenv("EC2_URL=https://ec2.us-west-2.amazonaws.com");
-        putenv("EC2_HOME=PATH_TO_TOOLS/ec2-api-tools-1.5.2.4/");
+        putenv("EC2_HOME=PATH_TO_EC2_TOOLS");
 
-        $EC2_TOOLS_PATH = "PATH_TO_TOOLS/ec2-api-tools-1.5.2.4/bin/";
-        $CERT='PATH TO AWS CERT';
-        $KEY='PATH TO AWS KEY';
-        $SNAP_DESCRIPTION = 'MY_VOLUME' . date("Y-m-d");
-        $VOLUME = 'AWS VOLUME ID';
+        $EC2_TOOLS_PATH = "PATH_TO_EC2_TOOLS";
+        $CERT='AWS_CERT';
+        $KEY='AWS_KEY';
+        $SNAP_DESCRIPTION = 'SNAPSHOT_DESCRIPTION' . date("Y-m-d");
+        $VOLUME = 'VOLUME_TO_BE_SNAPSHOTTED_ID (vol-xxxxxx)';
         $SNAP_TO_KEEP = 10;
+        $DB_HOST = "10.x.x.x.";
+        $DB_NAME = "";
+        $DB_USER = "";
+        $DB_PASS = "";
 
         //Make a snapshot
+        //TODO: Make an array of volumes and SNAP_TO_KEEP values and loop through script for each one.
         try {
-            $dbh = new PDO('mysql:host=MYSQL_HOST;dbname=DBNAME', 'USERNAME', 'PASSWORD');
+            $dbh = new PDO('mysql:host=' . $DB_HOST . ';' . 'dbname=' . $DB_NAME, $DB_USER, $DB_PASS);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected to database\n";
 
@@ -35,12 +45,10 @@
             $psQueryUnlock = $dbh->prepare("UNLOCK TABLES");
 
             echo "Flushing tables and locking them.\n";
-            //Setup for snapshot
             $psQueryFlush->execute();
             $psQueryFlushWithLock->execute();
 
             echo "Creating snapshot.\n";
-            //Create Snapshot
             $output = array();
             exec($EC2_TOOLS_PATH . 'ec2-create-snapshot ' . $VOLUME . ' -K ' . $KEY . ' -C ' . $CERT . ' -d ' . $SNAP_DESCRIPTION, $output);
             print_r($output);
@@ -48,7 +56,6 @@
             echo "unlocking tables.\n";
             $psQueryUnlock->execute();
 
-            //Close the connection
             $dbh = null;
 
             echo "done.\n";
@@ -61,7 +68,7 @@
         }
 
         //Now we delete old ones
-
+        echo "Looking if we need to delete old snapshots...";
         $output = array();
         exec($EC2_TOOLS_PATH . 'ec2-describe-snapshots -K ' . $KEY . ' -C ' . $CERT . " | grep " . $VOLUME, $output);
 
@@ -79,11 +86,11 @@
             $i++;
         }
 
-        //Sort that bad boy
         $snaps = mySort($snaps,0);
 
         //Delete if there are more than $SNAP_TO_KEEP
         if (sizeof($snaps) > $SNAP_TO_KEEP) {
+            echo "More than " . $SNAP_TO_KEEP . "deleting some snapshots..."
             for ($i = $SNAP_TO_KEEP; $i < sizeof($snaps); $i++) {
                 echo "Deleting snapshot " . $snaps[$i][1];
                 exec($EC2_TOOLS_PATH . 'ec2-delete-snapshot ' . $snaps[$i][1] . ' -K ' . $KEY . ' -C ' . $CERT);
